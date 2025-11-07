0575c0587fb88c169baaeac6ffda20a05fd744a7
Pedro Thiago <pedrothiagom456@gmail.com>
2025-11-07 11:34:49 -0300

    Exception handling - delete

--- src/main/java/com/javacourseexercise/course/services/UserService.java
+++ src/main/java/com/javacourseexercise/course/services/UserService.java
@@ -2,8 +2,12 @@ package com.javacourseexercise.course.services;
 
 import com.javacourseexercise.course.entites.User;
 import com.javacourseexercise.course.repositories.UserRepository;
+import com.javacourseexercise.course.services.exceptions.DatabaseException;
 import com.javacourseexercise.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,8 +32,15 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public void delete(Long id){
-        userRepository.deleteById(id);
+    public void delete(Long id) {
+
+        var userById = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id)); //Checks if the user exists by ID; if not, returns an exception.
+
+        try {
+            userRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User body){
